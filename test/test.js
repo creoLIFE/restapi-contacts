00d35463f6c4424b0681ae771ca50f85ff273126
Miroslaw Ratman <miroslawratman@mis-k1-3.local>
2015-04-16 00:57:59 +0200

    test query url update

--- test/test.js
+++ test/test.js
@@ -4,8 +4,7 @@
 
 var falkor = require('../node_modules/falkor')
 
-exports.testJsonContent = falkor.fetch('https://localhost:8080/access_token/mirek@ratman.pl/12345')
+exports.testJsonContent = falkor.fetch('https://localhost:8080/access_token/some.email@gmail.com/12345')
     .evaluateWithJsonBody(function (test, json) {
-console.log(json);
         test.equals(json.access_token, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MzAsImVtYWlsIjoibWlyZWtAcmF0bWFuLnBsIiwicmFuZG9tIjo2MzYxMDM4MjA5ODcuMDQ1OH0.zTzlMF-kWP7R4eHuApinN4RQ6yGUoHSoWbdciWkUEAU', 'This evaluator fails if we have work to do.')
     })
